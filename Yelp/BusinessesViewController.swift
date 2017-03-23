ce5a0b3107435780f2401831c21ff0ce2e1727d9
cjhong <cjhong6@gmail.com>
2017-03-22 21:58:18 -0700

    Add search bar in the navigation bar

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+class BusinessesViewController: UIViewController,UITableViewDataSource,UITableViewDelegate, UISearchBarDelegate {
     
     var businesses: [Business]!
     @IBOutlet weak var tabelView: UITableView!
+    var searchBar : UISearchBar?
+    var filteredBusinesses : [Business]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +22,14 @@ class BusinessesViewController: UIViewController,UITableViewDataSource,UITableVi
         tabelView.rowHeight = UITableViewAutomaticDimension
         tabelView.estimatedRowHeight = 120
         
-        Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
+        searchBar = UISearchBar()
+        searchBar?.sizeToFit()
+        navigationItem.titleView = searchBar
+        searchBar?.delegate = self
+        
+        Business.searchWithTerm(term: "Chinese", completion: { (businesses: [Business]?, error: Error?) -> Void in
             self.businesses = businesses
+            self.filteredBusinesses = businesses
             self.tabelView.reloadData()
             if let businesses = businesses {
                 for business in businesses {
@@ -33,16 +41,16 @@ class BusinessesViewController: UIViewController,UITableViewDataSource,UITableVi
             }
         )
         
-        /* Example of Yelp search with more search options specified
-         Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
-         self.businesses = businesses
-         
-         for business in businesses {
-         print(business.name!)
-         print(business.address!)
-         }
-         }
-         */
+        //Example of Yelp search with more search options specified
+//         Business.searchWithTerm(term: "Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
+//         self.businesses = businesses
+//         
+//         for business in businesses {
+//         print(business.name!)
+//         print(business.address!)
+//         }
+//         }
+
         
     }
     
@@ -52,7 +60,10 @@ class BusinessesViewController: UIViewController,UITableViewDataSource,UITableVi
     }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        if businesses != nil{
+        if filteredBusinesses != nil{
+            return filteredBusinesses.count
+        }
+        else if businesses != nil{
             return businesses.count
         }
         else{
@@ -63,11 +74,26 @@ class BusinessesViewController: UIViewController,UITableViewDataSource,UITableVi
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
         
-        cell.business = businesses[indexPath.row]
+        cell.business = filteredBusinesses[indexPath.row]
         
         return cell
     }
     
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredBusinesses is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+        filteredBusinesses = searchText.isEmpty ? businesses : businesses.filter { (item: Business) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            let businessName = item.name!
+            return businessName.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        
+        tabelView.reloadData()
+    }
     
     /*
      // MARK: - Navigation
